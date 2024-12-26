f2611b398454cae07090cf46a2b9d49cc4b6d72e
BHUVI1529 <srinivasanbhuvana7@gmail.com>
2024-12-26 12:17:03 +0530

    modified by BHUVI1529

--- src/main/java/com/example/attendance/repository/leaveRequestRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.example.attendance.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import com.example.attendance.model.leaveRequest;
-
-// @Repository
-// public interface leaveRequestRepository extends JpaRepository<leaveRequest, Long> {
-// }
